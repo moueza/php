718b93301725ffd833644f6d9a9c1a3abf9e80f9
moueza <mouezapeter@gmail.com>
2017-12-14 13:12:15 +0100

    sov

--- alone.php
+++ alone.php
@@ -1,28 +1,2 @@
-<!doctype=html>
-<html>
-  <meta utf8>
-  <head>
-  </head>
+echo "2";
 
-  <body>
-    <div>
-      <form method="POST" action="php.php">
-	<label href="mail">Votre email:</label>
-	<input type="text" name="mail"></input>
-  </form>
-    </div>
-      <!-- double col-->
-      <div class="reponses"></div>
-
-
-      <div id="doubling">
-	<?php
-
-	  ?>
-      </div>
-
-
-      
-  </body>
-
-<html>
---
+++ aloneOBJECT.php
@@ -0,0 +1,28 @@
+<!doctype=html>
+<html>
+  <meta utf8>
+  <head>
+  </head>
+
+  <body>
+    <div>
+      <form method="POST" action="php.php">
+	<label href="mail">Votre email:</label>
+	<input type="text" name="mail"></input>
+  </form>
+    </div>
+      <!-- double col-->
+      <div class="reponses"></div>
+
+
+      <div id="doubling">
+	<?php
+
+	  ?>
+      </div>
+
+
+      
+  </body>
+
+<html>
